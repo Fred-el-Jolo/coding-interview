8b0efa1ab46886c279f72e2b8108b37ad0f5efd9
Fred-el-Jolo <guillaume.frederic@gmail.com>
2017-12-12 16:23:22 +0100

    Update index.js

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,59 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// From 0 to n
+// Create empty string
+// Compute total string length
+// For 0 to string length
+// For each row :
+// - Compute number of dashes : row * 2 + 1 (always odd)
+// - Compute number of spaces : n - dashesNumber (always even)
+// - if column <= spacesNumber / 2
+//   - add a space
+// - else if column > spacesNumber / 2 && column <= dashesNumber + spacesNumber / 2
+//   - add a dash
+// - else if column < stringLength
+//   - add a space
+function pyramid(n) {
+    const midpoint = Math.floor((n * 2 - 1) / 2);
+    for (let row = 0; row < n; row++) {
+        let level = '';
+        for (let column = 0; column < n * 2 - 1; column++) {
+            if (midpoint - row <= column && midpoint + row >= column){
+                level += '#';
+            }
+            else {
+                level += ' ';
+            }
+        }
+        console.log(level);
+    }
+}
 
 module.exports = pyramid;
+
+// Fred's
+// From 0 to n
+// Create empty string
+// Compute total string length
+// For each row :
+// - Compute number of dashes : row * 2 + 1 (always odd)
+// - Compute number of spaces : n - dashesNumber (always even)
+// - Concatenate spacesNumber/2, dashesNumber, spacesNumber/2
+// function pyramid(n) {
+//     const fullLength = n * 2 - 1;
+//     for (let row = 0; row < n; row++) {
+//         let pyramidStr = '';
+//         let dashesNumber = row * 2 + 1;
+//         let spacesNumber = fullLength - dashesNumber;
+//         console.log(repeatChar(' ', spacesNumber/2) + repeatChar('#', dashesNumber) + repeatChar(' ', spacesNumber/2));
+//     }
+// }
+//
+// function repeatChar(char, times) {
+//     let result = '';
+//     for (let i=0; i < times; i++) {
+//         result += char;
+//     }
+//     return result;
+// }
