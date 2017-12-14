35e9c1609d20776cc1ae6bc1607ba877788b73fa
Fred-el-Jolo <guillaume.frederic@gmail.com>
2017-12-14 15:36:02 +0100

    Update index.js

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,91 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.stackOne = new Stack();
+        this.stackTwo = new Stack();
+    }
+
+    add (item) {
+        this.stackOne.push(item);
+    }
+
+    remove () {
+        while (this.stackOne.peek()) {
+            this.stackTwo.push(this.stackOne.pop())
+        }
+
+        const item = this.stackTwo.pop();
+
+        while (this.stackTwo.peek()) {
+            this.stackOne.push(this.stackTwo.pop());
+        }
+
+        return item;
+    }
+
+    peek () {
+        while (this.stackOne.peek()) {
+            this.stackTwo.push(this.stackOne.pop());
+        }
+
+        const item = this.stackTwo.peek();
+
+        while (this.stackTwo.peek()) {
+            this.stackOne.push(this.stackTwo.pop());
+        }
+
+        return item;
+    }
+}
 
 module.exports = Queue;
+
+// Fred's
+// TO EXPLAIN
+// class Queue {
+//     constructor() {
+//         this.stackOne = new Stack();
+//         this.stackTwo = new Stack();
+//     }
+
+//     add (item) {
+//         while (this.stackOne.peek()) {
+//             this.stackTwo.push(this.stackOne.pop());
+//         }
+
+//         this.stackOne.push(item);
+
+//         while (this.stackTwo.peek()) {
+//             this.stackOne.push(this.stackTwo.pop());
+//         }
+//     }
+
+//     remove () {
+//         while (this.stackOne.peek()) {
+//             this.stackTwo.push(this.stackOne.pop());
+//             if(!this.stackOne.peek()) {
+//                 break;
+//             }
+//         }
+
+//         while (this.stackTwo.peek()) {
+//             this.stackOne.push(this.stackTwo.pop());
+//         }
+
+//         return this.stackOne.pop();
+//     }
+
+//     peek () {
+//         while (this.stackOne.peek()) {
+//             this.stackTwo.push(this.stackOne.pop());
+//         }
+
+//         while (this.stackTwo.peek()) {
+//             this.stackOne.push(this.stackTwo.pop());
+//         }
+
+//         return this.stackOne.peek();
+//     }
+// }
