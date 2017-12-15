cdd67349208d69be4d87e9f5343035761b7ba03d
Fred-el-Jolo <guillaume.frederic@gmail.com>
2017-12-15 16:24:55 +0100

    Update index.js

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,39 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+// Validate left tree
+// set max to node.value
+// call validate on left value
+// Validate right tree
+// set min to node.value
+// call validate on right value
+function validate(node, min = null, max = null) {
+    if( (min && node.data < min) || (max && node.data > max) ) {
+        return false;
+    }
+
+    const leftIsValid = node.left ? validate(node.left, min, node.data) : true;
+    const rightIsValid = node.right ? validate(node.right, node.data, max) : true;
+
+    return leftIsValid && rightIsValid;
+}
 
 module.exports = validate;
+
+// Fred's
+// Validate left tree
+// set max to node.value
+// call validate on left value
+// Validate right tree
+// set min to node.value
+// call validate on right value
+// function validate(node, min = null, max = null) {
+//     if( (min && node.data < min) || (max && node.data > max) ) {
+//         return false;
+//     }
+
+//     const leftIsValid = node.left ? validate(node.left, min, node.data) : true;
+//     const rightIsValid = node.right ? validate(node.right, node.data, max) : true;
+
+//     return leftIsValid && rightIsValid;
+// }
